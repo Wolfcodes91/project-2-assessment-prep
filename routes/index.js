4392476e39b77fb0a78542c5f068a230c6f93cc7
Wolfcodes91 <Dave.M.Wolfberg@gmail.com>
2022-01-27 10:32:40 -0800

    DONE

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var cars = require('../data/cars');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { cars });
 });
+
+// Post /cars (create a car)
+router.post('/cars', function(req, res) {
+  req.body.fullTank = true;
+  cars.push(req.body);
+  res.redirect('/');
+})
+
+// DELETE /cars/:idx
+router.delete('/cars/:idx', function(req, res) {
+  cars.splice(req.params.idx, 1)
+  res.redirect('/')
+})
+
+
 
 module.exports = router;
